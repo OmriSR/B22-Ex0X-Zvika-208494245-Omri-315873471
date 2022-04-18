90270c4f471cd33a572ec83703a466d8deae2edc
ZvikaCohen <zvika96zvika@gmail.com>
2022-04-18 16:05:50 +0300

    Zvika added GameRules functions that support GameBoard data members.

--- Logic/GameBoard.cs
+++ Logic/GameBoard.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Logic
+namespace GameBoardNamespace
 {
     class GameBoard
     {
-        int size = 0;
+        private ushort[][] m_GameBoard; // 0 = blank, 1 = player1 = X, 2 = player2 = O
+        private readonly ushort r_BoardSize;
+
+        static void Main()
+        {
+            Console.WriteLine("Hayde");
+        }
+
+        public ushort GetItemOnPosition(ushort i_Row, ushort i_Col)
+        {
+            return m_GameBoard[i_Row][i_Col];
+        }
     }
+
+    
 }
--- Logic/GameRules.cs
+++ Logic/GameRules.cs
@@ -1,15 +1,128 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using GameBoardNamespace;
 
-namespace Logic
+namespace GameRulesNamespace
 {
     class GameRules
     {
-        public static void Main()
+        public static bool CheckValidMove(
+            ushort i_CurrentRow,
+            ushort i_CurrentCol,
+            ushort i_NewRow,
+            ushort i_NewCol,
+            ushort i_PlayerNumber,
+            ushort i_BoardSize,
+            ref bool i_PossibleEat,
+            GameBoard i_GameBoard)
         {
-            Console.WriteLine("Hayde");
-            Console.ReadLine();
+            bool theMoveIsValid = true;
+            i_PossibleEat = false;
+            theMoveIsValid = i_NewRow < i_BoardSize && i_NewCol < i_BoardSize;
+
+            // Was the given move RIGHT-UP?                                              
+            if(theMoveIsValid && i_CurrentCol - i_NewCol == -1 && i_CurrentRow - i_NewRow == 1)
+            {
+                theMoveIsValid = CheckValidMoveToRightUp(
+                    i_NewRow,
+                    i_NewCol,
+                    i_PlayerNumber,
+                    i_BoardSize,
+                    i_GameBoard,
+                    ref i_PossibleEat);
+            }
+
+            // Was the given move LEFT-UP?
+            if (theMoveIsValid && i_CurrentCol - i_NewCol == 1 && i_CurrentRow - i_NewRow == 1)
+            {
+                theMoveIsValid = CheckValidMoveToLeftUp(
+                    i_NewRow,
+                    i_NewCol,
+                    i_PlayerNumber,
+                    i_GameBoard,
+                    ref i_PossibleEat);
+            }
+
+            return theMoveIsValid;
+        }
+       
+        public static bool CheckValidMoveToRightUp(
+            ushort i_NewRow,
+            ushort i_NewCol,
+            ushort i_PlayerNumber,
+            ushort i_BoardSize,
+            GameBoard i_GameBoard, ref bool i_PossibleEat)
+        {
+            bool theMoveIsValid = true;
+            ushort itemInNextStep = i_GameBoard.GetItemOnPosition(i_NewRow, i_NewCol);
+
+                if (itemInNextStep == 0)
+                {
+                    theMoveIsValid = true;
+                }
+
+                else if (itemInNextStep != i_PlayerNumber && i_NewRow - 1 >= 0 && i_NewCol + 1 < i_BoardSize) // Is there an enemy in right up?
+                {
+
+                    if (i_GameBoard.GetItemOnPosition((ushort)(i_NewRow - 1), (ushort)(i_NewCol + 1)) == 0) // can I eat the enemy?
+                    {
+                        theMoveIsValid = true;
+                        i_PossibleEat = true;
+                    }
+
+                    else
+                    {
+                        theMoveIsValid = false;
+                        i_PossibleEat = false;
+                    }
+                }
+
+                else
+                {
+                    theMoveIsValid = false;
+                    i_PossibleEat = false;
+                }
+
+                return theMoveIsValid;
+        }
+
+
+        public static bool CheckValidMoveToLeftUp(
+            ushort i_NewRow,
+            ushort i_NewCol,
+            ushort i_PlayerNumber,
+            GameBoard i_GameBoard, ref bool i_PossibleEat)
+        {
+            bool theMoveIsValid = true;
+            ushort itemInNextStep = i_GameBoard.GetItemOnPosition(i_NewRow, i_NewCol);
+
+            if (itemInNextStep == 0)
+            {
+                theMoveIsValid = true;
+            }
+
+            else if (itemInNextStep != i_PlayerNumber && i_NewRow - 1 >= 0 && i_NewCol - 1 >= 0) // Is there an enemy in left up?
+            {
+
+                if (i_GameBoard.GetItemOnPosition((ushort)(i_NewRow - 1), (ushort)(i_NewCol - 1)) == 0) // can I eat the enemy?
+                {
+                    theMoveIsValid = true;
+                    i_PossibleEat = true;
+                }
+
+                else
+                {
+                    theMoveIsValid = false;
+                    i_PossibleEat = false;
+                }
+            }
+
+            else
+            {
+                theMoveIsValid = false;
+                i_PossibleEat = false;
+            }
+
+            return theMoveIsValid;
         }
     }
 }
